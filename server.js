1874a0c9f082016d069b399d65d2dd53028329f3
Cuw99 <dababihr@gmail.com>
2026-05-29 18:22:50 +0400

    Update server.js to use EJS and serve static files

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.send('موقع صفي يعمل بنجاح!');
+    res.render('index');
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`الموقع يعمل على المنفذ ${PORT}`));
